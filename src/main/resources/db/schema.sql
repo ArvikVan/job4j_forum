a8fed22af4ab051144b06fbd8a506d98f7162e6d
ArvikVan <arvikvan@gmail.com>
2022-02-15 00:11:16 +0300

    withBDopt

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -4,7 +4,7 @@ create table if not exists posts (
     description text,
     created timestamp without time zone not null default now()
 );
-/*CREATE TABLE if not exists authorities (
+CREATE TABLE if not exists authorities (
                                            id serial primary key,
                                            authority VARCHAR(50) NOT NULL unique
 );
@@ -15,4 +15,4 @@ CREATE TABLE if not exists users (
                                      password VARCHAR(100) NOT NULL,
                                      enabled boolean default true,
                                      authority_id int not null references authorities(id)
-);*/
+);
